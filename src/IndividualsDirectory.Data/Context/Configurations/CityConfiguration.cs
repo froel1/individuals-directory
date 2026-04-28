099870b6a041fb17a61196c809059c108ba30461
Tsotne Dundua <tsotne.dundua@singular.uk>
2026-04-28 09:55:55 +0400

    mc

--- src/IndividualsDirectory.Data/Context/Configurations/CityConfiguration.cs
+++ src/IndividualsDirectory.Data/Context/Configurations/CityConfiguration.cs
@@ -11,15 +11,5 @@ public class CityConfiguration : IEntityTypeConfiguration<City>
         builder.HasKey(x => x.Id);
         builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
         builder.HasIndex(x => x.Name).IsUnique();
-
-        builder.HasData(
-            new City {Id = 1, Name = "Tbilisi"},
-            new City {Id = 2, Name = "Batumi"},
-            new City {Id = 3, Name = "Kutaisi"},
-            new City {Id = 4, Name = "Rustavi"},
-            new City {Id = 5, Name = "Gori"},
-            new City {Id = 6, Name = "Zugdidi"},
-            new City {Id = 7, Name = "Poti"},
-            new City {Id = 8, Name = "Telavi"});
     }
 }
